4db7be21c0792ab1e51192901ec32c3ad3e00715
pfb2024 <pfb2024@info15.cshl.edu>
2024-10-17 12:12:32 -0400

    py6_part1

---
+++ py6/textanswers.py
@@ -0,0 +1,5 @@
+1a. the difference is that defining set() will consider each character alone, therefore in this case it
+becomes {'A','T','G'} but if you define as {'ATGAG'}, it considers that as one value in the list so it makes
+a list with one entry containing the entire 'ATGAG'
+1b. so yes it does matter
+1c. 3 for mySet, 1 for mySet2
